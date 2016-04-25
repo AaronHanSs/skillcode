a05b574fce8c106d91d2131466cee8060fe6336f
dupontbertrand <dupontbertrand59@gmail.com>
2016-04-26 01:26:29 +0200

    verification for same password

--- js/passwordverification.js
+++ js/passwordverification.js
@@ -46,5 +46,17 @@ $('.showpassword').hover(function () {
 }, function () {
    $('#password').attr('type', 'password');
 });
-
 })
+
+    function myFunction() {
+var pass1 = document.getElementById("password").value;
+var pass2 = document.getElementById("passwordconfirmation").value;
+if (pass1 != pass2) {
+    //alert("Passwords Do not match");
+    document.getElementById("password").style.borderColor = "#E34234";
+    document.getElementById("passwordconfirmation").style.borderColor = "#E34234";
+}
+else {
+    alert("OK meme pass");
+}
+}
