809b0df91a2feb7dfb18a86b0ba8b0f247a3ae7e
dupontbertrand <dupontbertrand59@gmail.com>
2016-04-25 21:57:50 +0200

    add button next question when the answer is correct on quiztest.html

--- js/main.js
+++ js/main.js
@@ -1,39 +1,43 @@
-
 // =========================  Quiz Panel animation  ============================
-$(function(){
-    var loading = $('#loadbar').hide();
-    $(document)
-    .ajaxStart(function () {
-        loading.show();
-    }).ajaxStop(function () {
-    	loading.hide();
+$(function() {
+  var hidebutton = $('#nextquestion').hide();
+  var loading = $('#loadbar').hide();
+  $(document)
+    .ajaxStart(function() {
+      loading.show();
+    }).ajaxStop(function() {
+      loading.hide();
     });
 
-    $("label.btn").on('click',function () {
-    	var choice = $(this).find('input:radio').val();
-    	$('#loadbar').show();
-    	$('#quiz').fadeOut();
-    	setTimeout(function(){
-           $( "#answer" ).html(  $(this).checking(choice) );
-            $('#quiz').show();
-            $('#loadbar').fadeOut();
-           /* something else */
-    	}, 1500);
+  $("label.btn").on('click', function() {
+    var choice = $(this).find('input:radio').val();
+    $('#loadbar').show();
+    $('#quiz').fadeOut();
+    setTimeout(function() {
+      $("#answer").html($(this).checking(choice));
+      $('#quiz').show();
+      $('#loadbar').fadeOut();
+      /* something else */
+    }, 1500);
+  });
+
+  $ans = 3;
+
+
+  $.fn.checking = function(ck) {
+    if (ck != $ans)
+      return 'INCORRECT';
+    else
+
+      $(".progress-quiz").css({
+      "width": "+20%"
     });
-
-    $ans = 3;
-
-
-    $.fn.checking = function(ck) {
-        if (ck != $ans)
-            return 'INCORRECT';
-        else
-
-            $(".progress-quiz").css({
- "width": "20%"
-});
-            return 'CORRECT';
-    };
+    $('#nextquestion').show();
+    return 'CORRECT';
+  };
 });
 // ======================== /Quiz Panel animation  ============================
- // Codemirror
+
+var questionstestquiz = ['Two is correct', 'Three is correct', 'One is correct', 'Three is correct'];
+var reponsestestquiz = ['2', '3', '1', '3']
+for (var i = 0; i < questionstestquiz.length; i += 1);
